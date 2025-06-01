8ccbbbc3261e2b7f37569c3e9348b454fdb1a511
Max <30201448+Sedowan@users.noreply.github.com>
2025-06-01 21:17:38 +0200

    Update web.h

--- main/web.h
+++ main/web.h
@@ -1,1 +1,25 @@
+#ifndef WEB_H
+#define WEB_H
+
+#include "esp_err.h"
+#include "esp_http_server.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Starts the HTTP server and registers all URI handlers, including root and WiFi manager endpoints.
+ */
+httpd_handle_t web_start_server(void);
+
+/**
+ * @brief HTML/JS/CSS frontend embedded as a C string array.
+ */
 extern const char index_html[];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WEB_H
